54350d89d049a8b16b65eaff26960eafd7769821
Iavor Orlyov <iavor_orlyov@yahoo.com>
2018-08-31 15:33:30 +0300

    #4 Updating PlayerPerformance added

--- MatchesHistory/MatchesHistory/Services/PerfomanceCalculationServices/CalculatePerformance.cs
+++ MatchesHistory/MatchesHistory/Services/PerfomanceCalculationServices/CalculatePerformance.cs
@@ -1,12 +1,13 @@
 ﻿namespace MatchesHistory.Services.PerfomanceCalculationServices
 {
+    using MatchesHistory.Data.Entities;
     using MatchesHistory.Models.Interfacecs;
     using MatchesHistory.Services.DatabaseServices;
     using System.Linq;
 
     public class CalculatePerformance : ICalculatePerformance
     {
-        IDatabaseService dbServide = new DatabaseService();
+        IDatabaseService dbService = new DatabaseService();
         private readonly string PRIVATE_ACCOUNT = "4294967295";
 
         public void UpdatePlayerInfo(long accountId)
@@ -14,13 +15,31 @@
             if (accountId.ToString() == PRIVATE_ACCOUNT)
                 return;
 
-            var results = dbServide.LastMonthResults();
+            var results = dbService.LastMonthResults();
 
-
-
+            PlayerPerformance currentPlayer = dbService.CurrentPlayer(accountId) == null ? new PlayerPerformance() : dbService.CurrentPlayer(accountId);
+            
             foreach (var result in results.Where(r => r.Players.Any(p => p.AccountId == accountId)))
             {
+                Player player = result.Players.FirstOrDefault(p => p.AccountId == accountId);
 
+                bool radiantWin = result.RadiantWin;
+
+                bool isPlayerRadiant = result.Players.Take(5).FirstOrDefault(p => p.AccountId == accountId) != null;
+
+                if ((radiantWin && isPlayerRadiant) || (!radiantWin && isPlayerRadiant))
+                    currentPlayer.Wins += 1;
+                else
+                    currentPlayer.Losses += 1;
+
+                currentPlayer.PlayedHeroes.Add(new PlayedHeroes()
+                {
+                    HeroId = player.HeroId,
+                    StartDate = result.StartTime
+                });
+
+                currentPlayer.GamesPlayedLastMonth += 1;
+                currentPlayer.GamesPlayedLastSixMonths += 1;
             }
         }
     }
